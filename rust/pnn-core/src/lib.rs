48a3227f7f385726f431b6ac7aca19240e3a0fbd
Jose Alejandro Montana <jamontanac@unal.edu.co>
2026-06-11 22:48:59 -0500

    exposing the methods to the lib

--- rust/pnn-core/src/lib.rs
+++ rust/pnn-core/src/lib.rs
@@ -4,6 +4,7 @@ pub mod model;
 pub mod predict;
 pub mod types;
 
+pub use model::{log_joint, log_likelihood, log_prior_beta, log_prior_k, log_sum_exp};
 pub use types::{
     CountTensor, DataMatrix, InferenceMethod, Labels, ModelError, PnnModel, PosteriorDraw,
     SamplerConfig,
